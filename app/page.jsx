4a2065f97c869c1b1086251ce8e4b83a590b05ad
parth-agrawall <yellor92@gmail.com>
2024-11-15 08:51:34 +0530

    updated apos changes

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           <div className = "text-center xl:text-left order-2 xl:order-none" >
             <span className = "text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br/> <span className = "text-accent">Parth Agrawal</span>
+              Hello {`I'm`} <br/> <span className = "text-accent">Parth Agrawal</span>
             </h1>
             <p className = "max-w-[500px] mb-9 text-white/80">
               I manage projects and I am proficient in
